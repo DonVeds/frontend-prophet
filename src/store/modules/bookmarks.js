97934b25807382d7b5240e632495d983d0dac1a0
Dmitry <donveds@gmail.com>
2020-02-16 15:00:08 +0300

    add check for bookmarked posts

--- src/store/modules/bookmarks.js
+++ src/store/modules/bookmarks.js
@@ -36,7 +36,6 @@ export default {
         // );
 
         state.bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
-        // console.log(state)
       }
     }
   },
--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -1,13 +1,22 @@
 export default {
   actions: {
-    async fetchPosts(ctx) {
+    async fetchPosts({ commit, rootState }) {
       const res = await fetch(
         "https://jsonplaceholder.typicode.com/posts?_limit=15"
       );
       const posts = await res.json();
-      
 
-      ctx.commit("updatePosts", posts);
+      for (let post in posts) {
+        if (rootState.bookmarks.bookmarkList) {
+          for (let bookmark in rootState.bookmarks.bookmarkList) {
+            if (rootState.bookmarks.bookmarkList[bookmark].id == posts[post].id) {
+              posts[post] = rootState.bookmarks.bookmarkList[bookmark];
+            }
+          }
+        }
+      }
+
+      commit("updatePosts", posts);
     }
   },
   mutations: {
